182329b89af15fff6cd8b36d04a3b54ac6533975
Gilbert <gmutai1194@gmail.com>
2025-05-21 23:26:30 +0300

    Add user and alerts functionality added

--- crmapp/views.py
+++ crmapp/views.py
@@ -1,35 +1,57 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm
 
 # Create your views here.
 
+# @login_required
 def home(request):
-    # Check to see if logging in
-    
-    if request.method =="POST":
+    return render(request, 'home.html')
+
+
+def login_user(request):
+    if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
-        # Authenticate
+        # Authenticate user
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             messages.success(request, "You have logged in successfully!")
-            return redirect('home')
+            return redirect('records')  # Redirect to home after login
         else:
-            messages.success(request, "There was an error logging in, Please try again!")
-            return redirect('home')
-    
+            messages.warning(request, "Invalid username or password. Please try again.")
+            return redirect('login')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'login.html')
 
-# def login_user(request):
-#     pass
 def logout_user(request):
 	logout(request)
-	messages.success(request, "You have been logged out!")
+	messages.warning(request, "You have been logged out!")
 	return redirect('home')
 
+@login_required
 def register_user(request):
-    return render(request, 'register.html', {})
-    
+    # Restrict access to admin users only
+    if not (request.user.is_staff or request.user.is_superuser):
+        messages.warning(request, "You are not authorized to perform this action!")
+        return redirect('records')
+
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "User successfully added!")
+            # Redirect admin to the records page after registration
+            return redirect('records') 
+    else:
+        form = SignUpForm()
+
+    return render(request, 'register.html', {'form': form})
+
+@login_required
+def customer_records(request):
+	return render(request, 'records.html')
+
