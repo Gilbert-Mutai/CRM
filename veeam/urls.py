4e5aa1bc9a13055bd6368f5e49f5c54f00408c26
Gilbert <gmutai1194@gmail.com>
2025-08-13 15:45:07 +0300

    Veeam module updated

--- veeam/urls.py
+++ veeam/urls.py
@@ -14,4 +14,9 @@ urlpatterns = [
         name="send_notification_veeam",
     ),
     path("export/", views.export_selected_records, name="export_selected_records"),
+    path('record/<int:pk>/tag/', views.update_veeam_tag, name='update_veeam_tag'),
+    path('record/<int:pk>/status/', views.update_veeam_status, name='update_veeam_status'),
+    path('record/<int:pk>/comment/', views.update_veeam_comment, name='update_veeam_comment')
+
+
 ]
